215a73aa008e1c548505239f2e121c734d7d32cf
Rishi <rishiekandala@gmail.com>
2024-02-08 13:40:20 +0530

    added list view builder and list tile widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,33 +19,22 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Colors.black,
-                  enabled: true,
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                  decoration: InputDecoration(
-                    filled: false,
-                    prefixIcon: Icon(Icons.email),
-                    fillColor: Colors.grey.withOpacity(0.3),
-                    hintText: 'Email',
-                    hintStyle:
-                        TextStyle(fontSize: 14, color: Colors.grey.shade600),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.red, width: 1),
-                      borderRadius: BorderRadius.circular(10)
+
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 50,
+                    itemBuilder: (context, index)
+                {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.black,
+                      backgroundImage: NetworkImage('https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY='),
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.green, width: 1),
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                  ),
-                  onChanged: (value) {
-                    print(value);
-                  },
-                ),
+                    title: Text('Rishie'),
+                    subtitle: Text('Full Stack Developer,App Developer'),
+                    trailing: Text('12:53 PM'),
+                  );
+                }),
               )
             ],
           )),
