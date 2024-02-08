e385fce80ee8075a960f0acaeff7d3fee5dac4ea
Rishi <rishiekandala@gmail.com>
2024-02-08 12:36:03 +0530

    added text form field widget

--- lib/main.dart
+++ lib/main.dart
@@ -19,20 +19,34 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              RichText(
-                  text: TextSpan(
-                      text: 'Hello',
-                      style: Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 50),
-                      children: [
-                    TextSpan(
-                        text: 'world',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 24)),
-                    TextSpan(
-                        text: 'flutter',
-                        style: TextStyle(
-                            fontWeight: FontWeight.normal, fontSize: 24)),
-                  ]))
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  cursorColor: Colors.black,
+                  enabled: true,
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  decoration: InputDecoration(
+                    filled: false,
+                    prefixIcon: Icon(Icons.email),
+                    fillColor: Colors.grey.withOpacity(0.3),
+                    hintText: 'Email',
+                    hintStyle:
+                        TextStyle(fontSize: 14, color: Colors.grey.shade600),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red, width: 1),
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 1),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
+              )
             ],
           )),
     );
