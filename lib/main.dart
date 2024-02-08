fc858f525ad66622a951897c595e65ff7a7bc44b
Rishi <rishiekandala@gmail.com>
2024-02-08 10:52:17 +0530

    expanded row widgets and container widget added

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Ten Widgets'),
+            backgroundColor: Colors.blue,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.red,
+                      height: 250,
+                      child: Center(child: Text('Container 1')),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.green,
+                      height: 250,
+                      child: Center(child: Text('Container 2')),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.pink,
+                      height: 250,
+                      child: Center(child: Text('Container 2')),
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.red,
+                        width: 1,
+                      ),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY='))),
+                  child: Center(child: Text('Hello Everyone')),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
